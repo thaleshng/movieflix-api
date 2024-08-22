5106ea9214ae3d6694ea80680d9a450de9a6946d
Thales Gomes <thaleshng@gmail.com>
2024-08-22 15:00:29 -0300

    configurações ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettierConfig,
+    {
+        plugins: { prettier: prettierPlugin },
+        rules: { "prettier/prettier": "error" },
+    },
+];
