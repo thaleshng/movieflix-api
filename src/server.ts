c77dc3e071b39147e6184e5d89a756afee53aa17
Thales Gomes <thaleshng@gmail.com>
2024-08-23 14:21:57 -0300

    Adicionada ordenação, gênero e idioma do filme

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,15 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get("/filmes", async (req, res) => {
-    const movies = await prisma.movie.findMany();
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc",
+        },
+        include: {
+            genres: true,
+            languages: true,
+        },
+    });
     res.json(movies);
 });
 
