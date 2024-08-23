b984f6a0a50a2169c91042a6a2aa00a85e1e69e6
Thales Gomes <thaleshng@gmail.com>
2024-08-23 13:53:41 -0300

    adicionada a rota de listagem

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/filmes", (req, res) => {
-    res.send("Listagem de Filmes");
+app.get("/filmes", async (req, res) => {
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
 });
 
 app.listen(port, () => {
